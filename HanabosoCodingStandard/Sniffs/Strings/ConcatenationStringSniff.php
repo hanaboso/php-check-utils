5381cb9cefbca89e09522342a232b2874eb037b6
Radek Jirsa <jirsa.r@ghanaboso.com>
2019-04-04 18:53:24 +0200

    Added own ConcatenationStringSniff

---
+++ HanabosoCodingStandard/Sniffs/Strings/ConcatenationStringSniff.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace HanabosoCodingStandard\Sniffs\Strings;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * Class ConcatenationStringSniff
+ *
+ * @package HanabosoCodingStandard\Sniffs\Strings
+ */
+final class ConcatenationStringSniff implements Sniff
+{
+
+    /**
+     * @var array
+     */
+    public $allowedConstants = ['__DIR__'];
+
+    /**
+     * @return array
+     */
+    public function register(): array
+    {
+        return [T_STRING_CONCAT];
+    }
+
+    /**
+     * @param File $file
+     * @param int  $position
+     */
+    public function process(File $file, $position): void
+    {
+        if (!in_array($file->getTokens()[$position - 2]['content'], $this->allowedConstants, TRUE)) {
+            $file->addError(
+                'Use sprintf() instead of concatenation with "."',
+                $position,
+                'HanabosoCodingStandard.Strings.ConcatenationString'
+            );
+        }
+    }
+
+}
