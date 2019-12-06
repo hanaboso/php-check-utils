29907fa28aa43c5ba924897bd511558a770e8b6c
Radek Jirsa <jirsa.r@hanaboso.com>
2019-12-06 10:44:23 +0100

    Composer updated

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-coverage=$(php vendor/bin/paratest -c ./vendor/hanaboso/php-check-utils/phpunit.xml.dist -p 4 --coverage-text --whitelist /var/www/src tests | grep -oE '^  Lines.+%' | grep -oE ' \d+' | grep -oE '\d+')
+coverage=$(php vendor/bin/paratest -c ./vendor/hanaboso/php-check-utils/phpunit.xml.dist -p 4 --coverage-text --whitelist src tests | grep -oE '^  Lines.+%' | grep -oE ' \d+' | grep -oE '\d+')
 
 if [[ $coverage -lt $1 ]];
 then echo -e "\033[1;31mSorry, your coverage is too low ($coverage% < $1%)!\033[0m"; exit 1;
