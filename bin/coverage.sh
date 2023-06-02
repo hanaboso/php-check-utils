01aaf8af2c2a971320f5a0c2a2b7e03cf0a37275
Radek Jirsa <jirsa.r@ghanaboso.com>
2023-06-02 13:40:19 +0200

    Composer updated

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -25,11 +25,11 @@ if [ "$whitelist" = '' ];
     then whitelist='src';
 fi
 
-if [ "$exclude" = '' ];
-    then exclude='unknown';
+if [ "$exclude" != '' ];
+    then exclude="--exclude-group $exclude";
 fi
 
-time -f "Elapsed time: %e s." php vendor/bin/paratest -c ./vendor/hanaboso/php-check-utils/phpunit.xml.dist -p $processes --coverage-text --coverage-filter $whitelist --exclude-group $exclude $tests | tee /tmp/phpunit-coverage.log
+time -f "Elapsed time: %e s." php vendor/bin/paratest -c ./vendor/hanaboso/php-check-utils/phpunit.xml.dist -p $processes --coverage-text --coverage-filter $whitelist $exclude $tests | tee /tmp/phpunit-coverage.log
 DISTR=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release);
 
 if [ $DISTR = 'alpine' ];
